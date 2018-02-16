3911c3a8ad03c5cec7ec6a2a5d5429f39907e221
Nandan <Nandan@192.168.1.8>
2018-02-16 23:49:22 +0530

    Added to index

---
+++ src/test/java/com/rentaladda/rent/config/DBRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.rentaladda.rent.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@PropertySource(value = {"classpath:application.properties"})
+public class DBRepositoryTest {
+
+	@Autowired
+	private Environment environment;
+	
+	public DBRepositoryTest(){
+	}
+	
+	@Bean
+	public DataSource getMySQLDataSource(){
+		
+		DriverManagerDataSource myDS = new DriverManagerDataSource();
+		myDS.setDriverClassName(environment.getRequiredProperty("jdbc.mysql.driverClassName"));
+		myDS.setUrl("jdbc.mysql.url");
+		myDS.setUsername("jdbc.mysql.username");
+		myDS.setPassword("jdbc.mysql.password");
+		return myDS;
+	}
+	
+	@Bean
+	public JdbcTemplate mySQLJdbcTemplate(DataSource getMySQLDataSource){
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(getMySQLDataSource);
+		jdbcTemplate.setResultsMapCaseInsensitive(true);
+        return jdbcTemplate;
+	}
+}
+
