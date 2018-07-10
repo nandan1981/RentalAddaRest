a0e8c0f1fee2ad951d1b5d1f83a9672636a39a4b
Nandan1981 <Nandan1981>
2018-07-11 03:32:14 +0530

    Code smell removal

--- src/test/java/com/rentaladda/rent/filter/ServletFilterTest.java
+++ src/test/java/com/rentaladda/rent/filter/ServletFilterTest.java
@@ -11,16 +11,16 @@ import javax.servlet.ServletResponse;
 public class ServletFilterTest implements javax.servlet.Filter {
 
 	public void destroy() {
-		
+		//To be implemented
 	}
 
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
 			throws IOException, ServletException {
-		
+		//To be implemented
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
-		
+		//To be implemented
 	}
 
 }
